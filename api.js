0dcbd119cc1c4d0fab3521c8161b20119b6a079c
Alex <a.french@mail.utoronto.ca>
2020-07-14 19:34:34 -0400

    Initial commit

---
+++ api.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    res.status(200).send();
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const api = require("./api");
+
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(cors({ origin: "http://localhost:8080", credentials: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "./public")));
+
+// Routes
+app.use("/api", api);
+
+// Database
+const dbUrl = process.env.DB_URL || "mongodb://localhost/ibmhack2020";
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true, useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (e) => {
+    console.log(e, "\nError connecting to database, exiting");
+    process.exit();
+});
+db.on("open", () => console.log(`Connected to database at ${dbUrl}`));
+
+// Start the server
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Started on port ${port}`));
