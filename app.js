c5a6f5fc191d5616ad40804c7947f801a07bb0cb
Alex <a.french@mail.utoronto.ca>
2020-07-16 12:49:25 -0400

    Add server support for processing captured images

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "5mb" }));
 app.use(express.static(path.join(__dirname, "./public")));
 
 // Routes
