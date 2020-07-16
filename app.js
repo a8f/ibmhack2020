14f2902b0189bcc3c822d32d11f800d38fb5ed85
Zhuoyue Lyu <Zhuoyue.Lyu@ibm.com>
2020-07-16 03:33:40 -0400

    model uploaded

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const VisualRecognitionV3 = require("watson-developer-cloud/visual-recognition/v3");
+const fs = require("fs");
 const api = require("./api");
 
 const app = express();
@@ -25,3 +27,23 @@ app.get("/:id", (req, res) => {
 // Start the server
 const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`Started on port ${port}`));
+
+// Classifier
+const visualRecognition = new VisualRecognitionV3({
+    version: "2018-03-19",
+    iam_apikey: "ijUdnh5zOWte6BT1d2ZPKDjOGunx6ottz7XxRVHxjSai",
+});
+
+const params = {
+    images_file: fs.createReadStream("/Users/zhuoyue/Desktop/1.jpg"),
+    classifier_ids: ["fullSource_317313030"],
+    threshold: 0.6,
+};
+
+visualRecognition.classify(params, (err, response) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(response, null, 2));
+    }
+});
