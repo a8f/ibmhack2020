12af5b79830fe823bfbde208431f1f794e5f84cc
Zhuoyue Lyu <Zhuoyue.Lyu@ibm.com>
2020-07-16 17:19:55 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ const fs = require("fs");
 
 const db = {};
 
-const savedCreds = JSON.parse(fs.readFileSync("db_creds.json"));
-const creds = { url: savedCreds.url, plugins: { iamauth: { iamApiKey: savedCreds.apikey } } };
+// const savedCreds = JSON.parse(fs.readFileSync("db_creds.json"));
+// const creds = { url: savedCreds.url, plugins: { iamauth: { iamApiKey: savedCreds.apikey } } };
+const creds = { url: "https://4e3044f8-e34f-463d-924a-638151ba13cf-bluemix.cloudantnosqldb.appdomain.cloud", plugins: { iamauth: { iamApiKey: "6UnH3Iw-MRytC2bUn6WhjS_1dgeeLo5OqO5_02Lsnit0" } } };
 Cloudant(creds, (err, cloudant) => {
     if (err) {
         console.log(`Error connecting to database: ${err.message}`);
